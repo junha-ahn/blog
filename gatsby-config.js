2e34dd38a1c584ecc682ffcc11a70c9e66971c70
안준하 <junha@coinone.com>
2020-11-09 17:32:53 +0900

    add: first post

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `바른자세 블로그`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Jun`,
+      summary: `who lives and works in South Korea.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
     },
   },
   plugins: [
